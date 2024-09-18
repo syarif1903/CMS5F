d6a998d93d5ad2ac72c0b9dc7abe281024c3b3a1
Alfisteven <118120118+Alfisteven@users.noreply.github.com>
2024-09-18 08:05:25 +0700

    Delete 00-contoh-theme directory

--- 00-contoh-theme/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-<p>&copy; <?php echo date('Y'); ?> Universitas Trunojoyo Madura. All rights reserved.</p>
-    <p>Alamat: Jl. Raya Telang PO Box 2 Kamal Bangkalan Madura, Indonesia</p>
-    <p>Email: info@trunojoyo.ac.id | Telepon: (031) 123-4567</p>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
--- 00-contoh-theme/sidebar.php
+++
@@ -1,5 +0,0 @@
-<aside>
-    <?php if (is_active_sidebar('main-sidebar')) : ?>
-        <?php dynamic_sidebar('main-sidebar'); ?>
-    <?php endif; ?>
-</aside>
